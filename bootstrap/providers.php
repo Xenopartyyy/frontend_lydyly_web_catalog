1433bf3b5a0aa474a11b9a9acf928de8f13f5aa1
Willyawan <willyawan.m@gmail.com>
2026-04-10 15:57:49 +0700

    delete unused library

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,6 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    Mckenziearts\Notify\LaravelNotifyServiceProvider::class,
     Maatwebsite\Excel\ExcelServiceProvider::class,
     Yajra\DataTables\DataTablesServiceProvider::class,
 
